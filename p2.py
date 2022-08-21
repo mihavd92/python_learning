4b91f133ee67ca3ac78fa99a9beb3db6d8df9ca7
Myhaylo Vdovychenko <50591995+mihavd92@users.noreply.github.com>
2022-08-21 15:51:40 +0300

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,3 +1,7 @@
+# На фермі є кури, корови та свині. 
+# Запросити у користувача кількість кур, свиней, корів 
+# та вивести на скільки всього ніг у тварин на фермі/
+
 chicken = int(input('Enter your number of Chickens'))
 cow = int(input('Enter your number of Cows'))
 pig = int(input('Enter your number of Pigs'))
